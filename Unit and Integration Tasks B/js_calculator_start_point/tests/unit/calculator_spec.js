55c3b1a3e3a97c6b942ccaecae28ed15c7e4461e
rurigrass <ruripg@gmail.com>
2018-08-08 20:50:08 +0100

    final commit

--- Unit and Integration Tasks B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,51 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('should add', function () {
+    calculator.previousTotal = 1;
+    calculator.add(4)
+    assert.equal(calculator.runningTotal, 5);
+});
+
+it('should subtract', function () {
+  calculator.previousTotal = 7;
+  calculator.subtract(4)
+  assert.equal(calculator.runningTotal, 3);
+});
+
+it('should multiply', function () {
+  calculator.previousTotal = 3;
+  calculator.multiply(5)
+  assert.equal(calculator.runningTotal, 15);
+});
+
+it('should divide', function () {
+  calculator.previousTotal = 21;
+  calculator.divide(7)
+  assert.equal(calculator.runningTotal, 3);
+});
+
+it('should concatenate multiple number on button click', function () {
+  calculator.numberClick(1);
+  calculator.numberClick(2);
+  calculator.numberClick(3);
+  assert.strictEqual(calculator.runningTotal, 123);
+});
+
+it('should chain operations together', function () {
+  calculator.numberClick(1);
+  calculator.operatorClick('+');
+  calculator.numberClick(5);
+  calculator.operatorClick('=');
+  assert.strictEqual(calculator.runningTotal, 6);
+});
+
+it('should clear the running total', function () {
+  calculator.numberClick(1);
+  calculator.numberClick(1);
+  calculator.numberClick(2);
+  calculator.clearClick()
+  assert.strictEqual(calculator.runningTotal, 0);
+});
+
 });
